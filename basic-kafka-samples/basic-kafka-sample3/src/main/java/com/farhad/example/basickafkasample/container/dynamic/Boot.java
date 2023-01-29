b3a02b590d8a2f1571499d2ad477486655d73a3f
Farhad rasouli <rasouli.farhad@gmail.com>
2023-01-29 08:43:09 +0330

    added example3

---
+++ basic-kafka-samples/basic-kafka-sample3/src/main/java/com/farhad/example/basickafkasample/container/dynamic/Boot.java
@@ -0,0 +1,63 @@
+package com.farhad.example.basickafkasample.container.dynamic;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.annotation.Order;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class Boot {
+    
+
+
+    @Bean
+    @Order(100)
+    public ApplicationRunner createContainer$MyListener(ConcurrentKafkaListenerContainerFactory<String,String> factory) {
+
+        return args -> {
+            ConcurrentMessageListenerContainer<String,String> container =  createContainer(factory, "my-topic", "my-group");
+        };
+
+
+    }
+
+    private ConcurrentMessageListenerContainer<String,String>  createContainer(
+                                        ConcurrentKafkaListenerContainerFactory<String,String> factory ,
+                                        String topic,
+                                        String  group) {
+
+        ConcurrentMessageListenerContainer<String,String> container = factory.createContainer(topic);
+
+        container.getContainerProperties().setMessageListener(new MyListener());
+        container.getContainerProperties().setGroupId(group);
+        container.setBeanName(group);
+        container.start();
+        return container ;
+
+    }
+
+
+    @Bean
+    @Order(200)
+    public ApplicationRunner sendData(KafkaTemplate<String,String> kafkaTemplate) {
+
+        return args -> {
+
+            for (int i = 0; i < 10; i++) {
+                log.info("Sending: {} ", "Message # " + i);
+                kafkaTemplate.send("my-topic","Message # " + i );
+            }
+        };
+
+
+    }
+
+
+}
---
+++ basic-kafka-samples/basic-kafka-sample3/src/main/java/com/farhad/example/basickafkasample/container/dynamic/MyListener.java
@@ -0,0 +1,17 @@
+package com.farhad.example.basickafkasample.container.dynamic;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MyListener implements MessageListener<String,String> {
+
+    @Override
+    public void onMessage(ConsumerRecord<String, String> consumerRecord) {
+        log.info("Received: {}",consumerRecord.value());
+        
+    }
+    
+}
